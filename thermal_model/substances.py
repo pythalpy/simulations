8c74d06d5c4167ac01cb6c331fa9812b9d90ee2d
Jaani <jaani@JSCorps-iMac-Pro.local>
2023-11-18 11:03:46 -0800

    updated getter and setter methods so that getter does not overwrite the mass after it is set with setter

--- thermal_model/substances.py
+++ thermal_model/substances.py
@@ -3,17 +3,24 @@ class Water:
         self.__mass = ''
 
     def getmass(self):
-        self.__mass = self.volume * self.density
+        if self.derived_mass:
+            self.__mass = self.volume * self.density
         return self.__mass
 
-    substanceName = 'Water'  # class attribute
+    def setmass(self, mass):
+        self.__mass = mass
+        print(f'new mass is set to: {mass}')
+        self.derived_mass = False
+
+    name = 'Water'  # class attribute
     specificHeat = 4.816  # KJ/(kg K) Specific Heat
     liquid_vapor_temp = 100
     heat_of_vaporization = 2256  # kJ/kg
     density = 1.000  # kg/L
     volume = 0  # L
     temp = 0
-    mass = property(getmass)
+    derived_mass = True
+    mass = property(getmass, setmass)
 
 
 class Milk:
@@ -21,14 +28,21 @@ class Milk:
         self.__mass = ''
 
     def getmass(self):
-        self.__mass = self.volume * self.density
+        if self.derived_mass:
+            self.__mass = self.volume * self.density
         return self.__mass
 
-    substanceName = 'Milk'  # class attribute
+    def setmass(self, mass):
+        self.__mass = mass
+        print(f'new mass is set to: {mass}')
+        self.derived_mass = False
+
+    name = 'Milk'  # class attribute
     specificHeat = 3.77  # KJ/(kg K) Specific Heat
     liquid_vapor_temp = 100.5
     heat_of_vaporization = 2256  # kJ/kg
     density = 1.035  # kg/L
     volume = 0  # L
-    mass = property(getmass)
+    derived_mass = True
+    mass = property(getmass, setmass)
     temp = 0
